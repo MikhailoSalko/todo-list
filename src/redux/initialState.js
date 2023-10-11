2757c9eba09b222ad8e69ee8e5473a65c6357f43
Mikhailo Salko <slipknotmisha@gmail.com>
2023-10-11 16:13:59 +0300

    update initial state for check render,  add selector and totoSlice

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -1,1 +1,28 @@
-export const initialState = { todoList: [] }
+export const initialState = {
+  todoList: [
+    {
+      id: 1,
+      title: 'Learn HTML',
+      description: 'This is the first test task',
+      completed: true,
+    },
+    {
+      id: 2,
+      title: 'Learn CSS',
+      description: 'This is the second test task',
+      completed: true,
+    },
+    {
+      id: 3,
+      title: 'Learn JS',
+      description: 'This is the third test task',
+      completed: false,
+    },
+    {
+      id: 4,
+      title: 'Learn React.js',
+      description: 'This is the fourth test task',
+      completed: false,
+    },
+  ],
+};
--- src/redux/todoSlice.js
+++ src/redux/todoSlice.js
@@ -1,21 +1,15 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { initialState } from './initialState';
-import { nanoid } from 'nanoid';
 
 const todoSlice = createSlice({
   name: 'todo',
   initialState,
   reducers: {
     add: (state, { payload }) => {
-      const objTodo = {
-        id: nanoid(),
-        completed: false,
-        title: payload,
-      };
-      state.todoList.push(objTodo);
+      state.todoList.push(payload);
     },
     remove: (state, { payload }) => {
-      state.todoList.filter(el => el.id !== payload);
+      state.todoList = state.todoList.filter(el => el.id !== payload);
     },
     update: (state, { payload }) => {
       const todo = state.todoList.findIndex(el => el.id === payload.id);
