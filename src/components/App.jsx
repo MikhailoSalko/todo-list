035692c75eebeb5346555e3a409eac8a4cdc1bd8
Mikhailo Salko <slipknotmisha@gmail.com>
2023-10-11 15:28:15 +0300

    first update App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,6 @@
+import TodoList from './Todolist/TodoList';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  const handleSubmit = () => {};
+  return <TodoList />;
 };
