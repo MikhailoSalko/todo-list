1f2643ae08ba7a4b3a864a3d7d250524baa36a71
Mikhailo Salko <slipknotmisha@gmail.com>
2023-10-13 09:23:41 +0300

    add first looking of modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,57 +1,85 @@
 import { nanoid } from '@reduxjs/toolkit';
 import React, { useState } from 'react';
+import { Button, Form, Modal } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
 import { add } from 'redux/todoSlice';
 
-const Modal = ({ handleCreate }) => {
+const ModalWindow = ({ isOpen, handleCloseModal }) => {
   const [nameTodo, setNameTodo] = useState('');
   const [descriptionTodo, setDescriptionTodo] = useState('');
+
+  const dispatch = useDispatch();
+
   const handleChange = ({ target }) => {
-    if (target.name === 'nameTodo') {
-      setNameTodo(target.value);
-    }
-    setDescriptionTodo(target.value);
+    target.name === 'nameTodo'
+      ? setNameTodo(target.value)
+      : setDescriptionTodo(target.value);
   };
-  const handleSubmit = e => {
+
+  const handleCreate = e => {
     e.preventDefault();
+    if (nameTodo.length < 4) {
+      return alert("the name of todo can't be empty or shorter then 3 symbols");
+    }
     const newTodo = {
       id: nanoid(),
       completed: false,
       title: nameTodo,
       description: descriptionTodo,
     };
-    add(newTodo);
+    dispatch(add(newTodo));
     setNameTodo('');
     setDescriptionTodo('');
+    handleCloseModal();
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="mb-3">
-        <label htmlFor="nameTodo" className="form-label">
-          To-Do Name:
-        </label>
-        <input
-          id="nameTodo"
-          name="nameTodo"
-          type="text"
-          className="form-control"
-          onChange={handleChange}
-          value={nameTodo}
-        />
-        <label htmlFor="descriptionTodo" className="form-label">
-          To-Do description:
-        </label>
-        <textarea
-          id="descriptionTodo"
-          name="descriptionTodo"
-          type="text"
-          className="form-control"
-          onChange={handleChange}
-          value={descriptionTodo}
-        />
-      </div>
-    </form>
+    <Modal backdrop={true} show={isOpen} onHide={handleCloseModal}>
+      <Modal.Header closeButton>
+        <Modal.Title>Add your todo</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Group className="mb-3">
+            <Form.Label>Add the name of todo</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Buy fresh vegetables"
+              autoFocus
+              name="nameTodo"
+              value={nameTodo}
+              onChange={handleChange}
+              required
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Add the description of todo</Form.Label>
+            <Form.Control
+              as="textarea"
+              name="descriptionTodo"
+              value={descriptionTodo}
+              rows={3}
+              onChange={handleChange}
+              placeholder="Tomatos, cucambers, potatoes"
+            />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleCloseModal}>
+          Close
+        </Button>
+        <Button
+          variant="primary"
+          disabled={nameTodo.length === 0}
+          type="submit"
+          onClick={handleCreate}
+        >
+          Save Changes
+        </Button>
+      </Modal.Footer>
+    </Modal>
   );
 };
 
-export default Modal;
+export default ModalWindow;
