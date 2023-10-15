9ffea00be6d67a860212c4e03589fd6adfff4eb9
Mikhailo Salko <slipknotmisha@gmail.com>
2023-10-15 17:12:18 +0300

    add ModalWindow component to TodoList, update Todo component

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { Button, Card, CloseButton, Form, ListGroup } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
 import { check, remove } from '../../redux/todo/todoSlice';
 
-const Todo = ({ todo }) => {
+const Todo = ({ todo, handleOpenModal }) => {
   const dispatch = useDispatch();
 
   const handleCheck = () => {
@@ -15,28 +14,30 @@ const Todo = ({ todo }) => {
   };
 
   return (
-    <ListGroup.Item as="li">
-      <Card>
-        <Card.Header className="d-flex justify-content-between align-items-start">
-          <Form.Check
-            inline
-            label={todo.title}
-            name={todo.title}
-            type="checkbox"
-            onChange={handleCheck}
-            checked={todo.completed}
-            id={todo.id}
-          />
-          <CloseButton aria-label="Close" onClick={handleDelete} />
-        </Card.Header>
-        <Card.Body>
-          <Card.Text className="mb-5">{todo.description}</Card.Text>
-          <Button variant="outline-primary" onClick={() => {}}>
-            Update todo
-          </Button>
-        </Card.Body>
-      </Card>
-    </ListGroup.Item>
+    <>
+      <ListGroup.Item as="li">
+        <Card>
+          <Card.Header className="d-flex justify-content-between align-items-start">
+            <Form.Check
+              inline
+              label={todo.title}
+              name={todo.title}
+              type="checkbox"
+              onChange={handleCheck}
+              checked={todo.completed}
+              id={todo.id}
+            />
+            <CloseButton aria-label="Close" onClick={handleDelete} />
+          </Card.Header>
+          <Card.Body>
+            <Card.Text className="mb-5">{todo.description}</Card.Text>
+            <Button variant="outline-primary" onClick={handleOpenModal}>
+              Update todo
+            </Button>
+          </Card.Body>
+        </Card>
+      </ListGroup.Item>
+    </>
   );
 };
 
--- src/components/Todolist/TodoList.jsx
+++ src/components/Todolist/TodoList.jsx
@@ -1,17 +1,38 @@
 import { useSelector } from 'react-redux';
 import { ListGroup } from 'react-bootstrap';
 import Todo from 'components/Todo/Todo';
-import { selectTodoList } from 'redux/todo/todoSelectors';
+import useModalState from 'hooks/useModalState';
+import ModalWindow from 'components/Modal/Modal';
+import { selectFilteredTodos } from 'redux/filter/filterSelectors';
 
-const TodoList = () => {
-  const todoList = useSelector(selectTodoList);
+const TodoList = ({
+  nameTodo,
+  descriptionTodo,
+  handleChange,
+  handleUpdateTodo,
+}) => {
+  const [isOpen, , handleOpenModal, handleCloseModal] = useModalState(false);
+  const todoList = useSelector(selectFilteredTodos);
 
   return (
     <>
       <ListGroup as="ul">
-        {todoList.length > 0 &&
-          todoList.map(todo => <Todo key={todo.id} todo={todo} />)}
+        {todoList?.length > 0 &&
+          todoList.map(todo => (
+            <Todo key={todo.id} todo={todo} handleOpenModal={handleOpenModal} />
+          ))}
       </ListGroup>
+      {isOpen && (
+        <ModalWindow
+          modalTitle={'Update your todo'}
+          isOpen={isOpen}
+          nameTodo={nameTodo}
+          descriptionTodo={descriptionTodo}
+          handleChange={handleChange}
+          handleCloseModal={handleCloseModal}
+          handleSubmit={handleUpdateTodo}
+        />
+      )}
     </>
   );
 };
